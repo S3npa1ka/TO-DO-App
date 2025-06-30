2a63a6b5333bc9955196b04268bd19450d4e20c7
Ilia <mashkovjp@gmail.com>
2025-06-30 12:55:30 +0900

    Add sumple gui functions without enteraction

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
 inputBox = sg.InputText(tooltip= " Enter to do")
 add_button = sg.Button("Add")
+
 window = sg.Window('My TO-DO App', layout=[[label], [inputBox, add_button]])
 window.read()
 window.close()
