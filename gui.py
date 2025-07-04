43cef5dfeb49095b7459a40c8e9ef906910b30e5
Ilia <mashkovjp@gmail.com>
2025-07-04 21:31:01 +0900

    Implement Edit functions and real time update

--- gui.py
+++ gui.py
@@ -1,12 +1,16 @@
+from gc import enable
+
 import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
 inputBox = sg.InputText(tooltip= " Enter to do", key="todo")
 add_button = sg.Button("Add")
-
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 window = sg.Window('My TO-DO App',
-                   layout=[[label], [inputBox, add_button],],
+                   layout=[[label], [inputBox, add_button],[list_box, edit_button]],
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -18,6 +22,21 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo + "\n"
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+
+
         case sg.WIN_CLOSED:
             break
 window.close()
