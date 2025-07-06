72e31fe2e46afcbff7c0e946e4794ceeb01bb1b5
Ilia <mashkovjp@gmail.com>
2025-07-07 07:45:34 +0900

    Aded complete function

--- gui.py
+++ gui.py
@@ -4,13 +4,17 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-inputBox = sg.InputText(tooltip= " Enter to do", key="todo")
+inputBox = sg.InputText(tooltip="Enter to do", key="todo")
 add_button = sg.Button("Add")
+complete_button = sg.Button("Complete")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+layout = [[label],
+          [inputBox, add_button],
+          [list_box, edit_button, complete_button]]
 window = sg.Window('My TO-DO App',
-                   layout=[[label], [inputBox, add_button],[list_box, edit_button]],
+                   layout=layout,
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -35,6 +39,13 @@ while True:
             window['todos'].update(values=todos)
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+        case 'Complete':
+            todos = functions.get_todos()
+            todo_to_delete = values['todos'][0]
+            todos.remove(todo_to_delete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
 
 
         case sg.WIN_CLOSED:
