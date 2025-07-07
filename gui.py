299feb19d4b07d6fd86842e1099130430ba09825
Ilia <mashkovjp@gmail.com>
2025-07-07 12:27:23 +0900

    Impliment exit button and fix the prioblem with text in input box

--- gui.py
+++ gui.py
@@ -6,13 +6,15 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
 inputBox = sg.InputText(tooltip="Enter to do", key="todo")
 add_button = sg.Button("Add")
+exit_button = sg.Button("Exit")
 complete_button = sg.Button("Complete")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 layout = [[label],
           [inputBox, add_button],
-          [list_box, edit_button, complete_button]]
+          [list_box, edit_button, complete_button],
+          [exit_button]]
 window = sg.Window('My TO-DO App',
                    layout=layout,
                    font=('Helvetica', 20))
@@ -45,8 +47,9 @@ while True:
             todos.remove(todo_to_delete)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-
-
+            window['todo'].update(value='')
+        case 'Exit':
+            break
 
         case sg.WIN_CLOSED:
             break
