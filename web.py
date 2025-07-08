3ab48eb444fff70b265d068160af132104523838
Ilia <mashkovjp@gmail.com>
2025-07-08 21:35:08 +0900

    implement complete feature

--- web.py
+++ web.py
@@ -1,4 +1,6 @@
 import streamlit as st
+
+
 import functions
 
 
@@ -14,8 +16,13 @@ st.title("MY TODO APP")
 st.subheader("My first web App")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add a todo...",
               on_change=add_todo, key='new_todo')
