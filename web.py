7c74b37e08bd66e2bf7d513ff07540cf9fa01f85
S3npa1ka <mashkov.jp@gmail.com>
2025-07-13 14:21:54 +0900

    Add confirmation step before removing tasks

--- web.py
+++ web.py
@@ -5,11 +5,20 @@ import functions
 
 
 todos = functions.get_todos()
+
+
 def add_todo():
     todo = st.session_state['new_todo'].strip() + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
+
+def complete_todo(index: int, todo_text: str) -> None:
+    """Remove the selected todo item once the user confirms."""
+    todos.pop(index)
+    functions.write_todos(todos)
+    del st.session_state[todo_text]
+    st.rerun()
 
 
 st.title("MY TODO APP")
@@ -19,13 +28,14 @@ st.write("This app is to increase your productivity")
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
-        todos.pop(index)
-        functions.write_todos(todos)
-        del st.session_state[todo]
-        st.rerun()
+        st.button(
+            "Complete",
+            key=f"complete_{index}",
+            on_click=complete_todo,
+            args=(index, todo),
+        )
 
-st.text_input(label="", placeholder="Add a todo...",
-              on_change=add_todo, key='new_todo')
+st.text_input(label="", placeholder="Add a todo...", on_change=add_todo, key="new_todo")
 
 st.session_state
 
